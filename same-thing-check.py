a72e9ad0e6dabb3592f267dc0bd7f2d2691636a6
TekCris <153227813+herendicristi@users.noreply.github.com>
2024-10-06 20:45:19 +0100

    Add files via upload

---
+++ same-thing-check.py
@@ -0,0 +1,9 @@
+#little shit thing i made to mainly check hashes
+print("same thing checker")
+original = input("1st thing: ")
+second = input("2nd thing: ")
+
+if original == second:
+    print(True)
+else:
+    print(False)
